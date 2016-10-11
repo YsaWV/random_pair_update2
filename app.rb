54fa2568efc982d22714f184b54701048a97bcd6
Ysabel Bombardiere <ysabelestradab@gmail.com>
2016-10-11 13:54:09 -0400

    app names

--- app.rb
+++ app.rb
@@ -3,11 +3,25 @@ require_relative 'random_pair.rb'
 require_relative 'formatter.rb'
 
 get '/' do
-    erb :get_names
+    erb :get_class_size
+    #erb :get_names
 end
 
+post '/class_size' do
+ 	students_n = params[:students]
+ 	redirect '/names?students=' + students_n
+end
+
+get '/names' do
+	class_size = params[:students]
+	
+	erb :get_names, :locals => {:class_size => class_size}
+end
+
+
+
 post '/names' do
-    names = params[:user_names].split.map(&:capitalize).join(' ')
+    names = params[:user_names]
     random_names_array = randomizer(names)
     name_pairs = fix(random_names_array)
     erb :get_names_again, :locals => {:name_pairs => name_pairs}
---
+++ name_loop.rb
@@ -0,0 +1,15 @@
+
+
+def loopname(names)
+    i =0
+    storename = []
+    
+        for i in 1..9 do
+            storename << names  
+            i =+
+            #break if i == 8
+        
+        end
+end
+
+loopname("x")
